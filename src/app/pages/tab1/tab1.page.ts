51afe8a6d8a6322d6f64808b122289b39e028a19
Pablo Ortiz Barra <portiz@dgtm.cl>
2019-04-23 17:28:02 -0400

    service y modelo

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-tab1',
@@ -13,7 +14,8 @@ export class Tab1Page {
     allowSlideNext: false
   };
 
-  constructor(private barcodeScanner: BarcodeScanner){}
+  constructor(private barcodeScanner: BarcodeScanner,
+              private dataLocal: DataLocalService){}
   ionViewDidEnter(){
     console.log('viewdidenter');
     
@@ -33,8 +35,13 @@ export class Tab1Page {
   scan(){
     this.barcodeScanner.scan().then(barcodeData => {
       console.log('Barcode data', barcodeData);
+      if (!barcodeData.cancelled) {
+        this.dataLocal.guardarRegistro(barcodeData.format, barcodeData.text);
+      }
+
      }).catch(err => {
          console.log('Error', err);
+         this.dataLocal.guardarRegistro('QRCode', 'http://www.google.cl');
      });
   }
 }
--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  constructor(public dataLocal: DataLocalService){}
+
+  enviarCorreo(){
+
+    console.log('Enviando Correo....');
+    
+
+  }
+
+  abrirRegistro(registro){
+    
+  }
+
+}
