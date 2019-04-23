680b68414328901ca124ef991c493e85e59ae810
Pablo Ortiz Barra <portiz@dgtm.cl>
2019-04-23 16:22:27 -0400

    first commit

---
+++ src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  swiperOpts = {
+    allowSlidePrev: false,
+    allowSlideNext: false
+  };
+
+  constructor(private barcodeScanner: BarcodeScanner){}
+  ionViewDidEnter(){
+    console.log('viewdidenter');
+    
+  }
+
+  ionViewDidLeave(){
+    console.log('ionViewDidLeave');
+    
+  }
+
+  ionViewWillEnter(){
+    console.log('viewWillEnter');
+    this.scan();
+    
+  }
+
+  scan(){
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
+}
